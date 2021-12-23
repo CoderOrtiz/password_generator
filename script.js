3fe9af06ae7ad5186957406663a5d7b44065d7d9
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2021-12-22 23:28:36 -0500

    edits done. submitted mid-edit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 var generateButton = document.querySelector ("#generate");
 
-function generatePassword (){
-  var specialCharaters = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "[", "]", "~", "-", "_", "."];
+
+var specialCharaters = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "[", "]", "~", "-", "_", "."];
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCasedCharacters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"]
 var upperCasedCharacters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -22,43 +22,35 @@ var special = confirm ("Last option human, click Okay if you would like lowercas
 
 // conditions of the array
 
-if (numbers){
-  resultArray = resultArray.concat(numericCharacters);
-} 
-if (upper){
-  resultArray = resultArray.comcat(upperCasedCharacters);
-}
-if (lower){
-  resultArray = resultArray.comcat(lowerCasedCharacters);
-}
-if (special){
-  resultArray = resultArray.comcat(specialCharaters);
+var random = document.querySelector("#generate");
+
+random.addEventListener("click", function(){
+  randomPassword = generatePassword();
+  document.getElementById("password").placeholder = randomPassword;
+});
+
+function generatePassword() {
+  enter = parseInt(prompt("How long would you like your password to be? Please select 8 - 128 characters."))
 }
 
-console.log(resultArray)
+if (!enter) {
+  alert("HEY! It needs a value...");
+} else if (enter < 8 || enter > 128) {
+    enter = parseInt(prompt("Hey! I said to choose between 8 to 128 characters human."));
 
-for (var i=0; i < numericCharacters; i++){
+} else {
+  confirmNumber = confirm ("How many characters you want? Choose between 8 and 128?")
+  confirmCharacter = confirm("Do want special characters in your password?")
+  confirmUppercase = confirm("Do want Uppercases in your password?")
+  confirmLowercase = confirm("Do you want lowercases in your password?")
+};
 
-  userArray.push (resultArray[Math.floor(math.random() * resultArray)]);
+if (!confirmCharacter && !confirmNumber && !confirmUppercse && !confirmLowercase) {
+  choices = alert("Choose one of them!")
+}
+// 
+else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+  choices = character.concat(specialCharaters, numericCharacters, lowerCasedCharacters, upperCasedCharacters)
 }
 
-return userArray.join("")
-}
-
-// make password
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("password");
-  
-  passwordText.value = password;
-}
-
-function copyToClipboard() {
-
-}
-
-generateButton.addEventListener("click", writePassword);
-
-
-
+else if (confirmCharacter && confirmNumber && confirmUppercase)
